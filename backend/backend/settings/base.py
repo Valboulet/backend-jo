995068beeb12efcf2599bdd29da737662a4eefbb
Valboulet <valboulet59@gmail.com>
2024-09-24 15:16:38 +0200

    Updated readme.md and dockerfiles

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -15,9 +15,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
+# SECRET_KEY = 'imx&_^!+vaho5rr8afcds#-yb@k-tli=6j-2w$zu^75zhwg+y*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=False)
+# DEBUG = 1
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # ALLOWED_HOSTS = ['*']
@@ -112,6 +114,17 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'jo2024',
+#         'USER': 'joadmin',
+#         'PASSWORD': '12PoisHaie74?',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
